41e208c01b17893763f6e5ffb1ad86bfed695ecd
tanvirr007 <md.tanvirhasannijum@gmail.com>
2024-12-16 03:15:58 +0600

    py: add folder creation and JSON saving functions
    Change-Id: I7152dd55855ab339b8142bca2a1153a4a434d2ba
    Signed-off-by: tanvirr007 <tanvir.mymnsngh@gmail.com>

--- bot.py
+++ bot.py
@@ -1,3 +1,22 @@
 import os
 import json
 import requests
+
+def create_data_folder(username):
+    """
+    Creates a folder named after the GitHub username in the 'data' directory.
+    """
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+    user_folder = os.path.join('data', username)
+    if not os.path.exists(user_folder):
+        os.makedirs(user_folder)
+    return user_folder
+
+def save_to_json(file_path, data):
+    """
+    Saves data to a JSON file.
+    """
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
